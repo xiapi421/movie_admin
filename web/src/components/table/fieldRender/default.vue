263a5c9ff8533d0fa732de253b9263ee7d9ebb37
xiapi421 <xiapi421@gmail.com>
2025-03-04 19:46:24 +0800

    Apply .gitignore rules

--- web/src/components/table/fieldRender/default.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <el-tag effect="dark" type="danger">Field renderer not found</el-tag>
-    </div>
-</template>
--- web/src/lang/backend/en/login.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    'Please enter an account': 'Please enter your account',
-    'Please input a password': 'Please enter your password',
-    'Hold session': 'Keep the session',
-    'Sign in': 'Sign in',
-}
