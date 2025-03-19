21e3fa53648263c902c216fc43ca308260d1a454
xiapi421 <xiapi421@gmail.com>
2025-03-19 21:53:07 +0800

    sda

--- app/admin/controller/Bucket.php
+++ app/admin/controller/Bucket.php
@@ -3,7 +3,8 @@
 namespace app\admin\controller;
 
 use app\common\controller\Backend;
-
+use Throwable;
+use ba\Bce;
 /**
  * 储存桶管理
  */
@@ -26,6 +27,41 @@ class Bucket extends Backend
         $this->model = new \app\admin\model\Bucket();
     }
 
+    public function del(): void
+    {
+        $where             = [];
+        $dataLimitAdminIds = $this->getDataLimitAdminIds();
+        if ($dataLimitAdminIds) {
+            $where[] = [$this->dataLimitField, 'in', $dataLimitAdminIds];
+        }
+
+        $ids     = $this->request->param('ids/a', []);
+        $where[] = [$this->model->getPk(), 'in', $ids];
+        $data    = $this->model->where($where)->select();
+
+        $count = 0;
+        $this->model->startTrans();
+        try {
+            foreach ($data as $v) {
+                $bce = new Bce([
+                    'accessKeyId' => $v['apiKey'],
+                    'secretAccessKey' => $v['secret'],
+                ]);
+                $bce->deleteFile($v['name'],$v['filename']);
+                $bce->deleteBucket($v['name']);
+                $count += $v->delete();
+            }
+            $this->model->commit();
+        } catch (Throwable $e) {
+            $this->model->rollback();
+            $this->error($e->getMessage());
+        }
+        if ($count) {
+            $this->success(__('Deleted successfully'));
+        } else {
+            $this->error(__('No rows were deleted'));
+        }
+    }
 
     /**
      * 若需重写查看、编辑、删除等方法，请复制 @see \app\admin\library\traits\Backend 中对应的方法至此进行重写
